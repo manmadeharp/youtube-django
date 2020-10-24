9893e197846f359e8af697a3bb1fe7ba21b7971b
Finbar <finbarccowan@gmail.com>
2020-10-24 12:13:36 +0100

    (new commit)

--- youtube/youtube_python/forms.py
+++ youtube/youtube_python/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Your name', max_length=50);
-    password = forms.CharField(label='Your password', max_length=50);
+    username = forms.CharField(label='Username', max_length=50);
+    password = forms.CharField(label='Password', max_length=50);
 
 class RegisterForm(forms.Form):
     username = forms.CharField(label='Username', max_length=50);
     password = forms.CharField(label='Password', max_length=50);
     email = forms.CharField(label='Email', max_length=50);
+
+class NewVideoForm(forms.Form):
+    title = forms.CharField(label='Title', max_length=50);
+    description = forms.Textarea();
+    file = forms.FileField();
+
+
--- youtube/youtube_python/views.py
+++ youtube/youtube_python/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login, logout
 from django.views.generic.base import View, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, NewVideoForm
 from django.contrib.auth.models import User
 
 class HomeView(View):
@@ -8,25 +9,45 @@ class HomeView(View):
 
     def get(self, request):
         variableA = 'title'
-        return render(request, self.template_name, {'variableA': variableA})
+        return render(request, self.template_name, {'menu_active_item': 'home'})
 
 
 class LoginView(View):
     template_name = 'login.html'
 
     def get(self, request):
+        if request.user.is_authenticated:
+            print('already logged in. Redirecting')
+            print(request.user)
+            logout(request)
+            return HttpResponseRedirect('/')
         form = LoginForm()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         print(request)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print('successful login')
+                return HttpResponseRedirect('/')
+            else:
+                print('login failed')
+                return HttpResponseRedirect('/login')
         return HttpResponse('this is login view this is POST request')
 
 
 class RegisterView(View):
     template_name = 'register.html'
-
     def get(self, request):
+        if request.user.is_authenticated:
+            print('already logged in. Redirecting')
+            print(request.user)
+            return HttpResponseRedirect('/')
         form = RegisterForm()
         return render(request, self.template_name, {'form': form})
 
@@ -54,5 +75,5 @@ class NewVideo(View):
 
     def get(self, request):
         variableA = 'new video'
-        form = FormClass()
+        form = NewVideoForm()
         return render(request, self.template_name, {'variableA': variableA, 'form': form})
