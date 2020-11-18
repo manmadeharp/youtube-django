afc91039f3bc5c6bcdb1955b0e2d1efc648f174a
Finbar <finbarccowan@gmail.com>
2020-11-18 16:48:20 +0000

    completed project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Youtube',
+    version='',
+    packages=['youtube', 'youtube.migrations', 'youtube_python'],
+    url='',
+    license='',
+    author='Finbar Cowan',
+    author_email='',
+    description=''
+)
